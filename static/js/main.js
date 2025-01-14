d868793b3490dd5fc84f13bd9ba34a5d2b095860
TurboLimon <turbolimonplay@gmail.com>
2025-01-14 17:09:38 +0400

    Update scroll

--- static/js/main.js
+++ static/js/main.js
@@ -1,12 +1,15 @@
 function scroll_to_id() {
-    $('a[href^="#"]').bind('click.smoothscroll',function (e) {
-      e.preventDefault();
-      var target = this.hash,
-      $target = $(target);
-      $('html, body').stop().animate({
-        'scrollTop': $target.offset().top - 50
-      }, 500, 'swing', function () {
-        window.location.hash = target;
-      });
+    $('a[href^="#"]').on('click.smoothscroll', function(e) {
+        e.preventDefault();
+        var target = this.hash,
+            $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top - 150
+        }, 500, 'swing');
     });
 }
+
+// Инициализируем функцию при загрузке DOM
+$(document).ready(function() {
+    scroll_to_id();
+});
